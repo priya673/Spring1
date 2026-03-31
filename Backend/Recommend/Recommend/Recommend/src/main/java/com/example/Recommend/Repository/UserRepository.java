217a9f46ee3c9d53b6ac2a87130db59c6090916a
priya673 <124548175+priya673@users.noreply.github.com>
2026-03-31 11:30:26 +0530

    Delete Backend/Recommend/Recommend/Recommend directory

--- Backend/Recommend/Recommend/Recommend/src/main/java/com/example/Recommend/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Recommend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Recommend.Model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmail(String email);
-}
